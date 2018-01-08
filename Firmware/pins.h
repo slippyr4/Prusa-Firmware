5973368332d4386a0e1bedaa6a2cb70705cf5507
PabloGNU <pablo@lanzate.com>
2018-01-08 17:39:39 +0100

    GT2560 REV A+ PINS
    All PIN Definitons por GT2560 A+.

--- Firmware/pins.h
+++ Firmware/pins.h
@@ -455,7 +455,123 @@
   
 #endif
 
+#if MOTHERBOARD == BOARD_GT2560_REV_A_PLUS
+  #define ELECTRONICS "MKS Base"
+  #define KNOWN_BOARD
+  #ifndef __AVR_ATmega2560__
+    #error Oops!  Make sure you have 'Arduino Mega 2560' selected from the 'Tools -> Boards' menu.
+  #endif
 
+  // filament runout sensor (NOT SUPPORTED)
+  #define FR_SENS 21
+
+  // prusa multimaterial support (NOT SUPPORTED)
+  #ifdef SNMM
+  
+  #define E_MUX0_PIN 17
+  #define E_MUX1_PIN 16
+  #define E_MUX2_PIN 84
+  
+  #endif
+  
+  #define LARGE_FLASH true
+
+  // core pins
+  #define X_STEP_PIN 25 //
+  #define X_DIR_PIN 23 // 
+  #define X_MIN_PIN 22 //
+  #define X_MAX_PIN 24 //
+  #define X_ENABLE_PIN 27 //
+
+  #define Y_STEP_PIN 31 // 
+  #define Y_DIR_PIN 33 //
+  #define Y_MIN_PIN 26 //
+  #define Y_MAX_PIN 28 //
+  #define Y_ENABLE_PIN 29 //
+  
+  #define Z_STEP_PIN 37 //
+  #define Z_DIR_PIN 39 //
+  #define Z_MIN_PIN 30 //
+  #define Z_MAX_PIN 32 //
+  #define Z_ENABLE_PIN 35 //
+  
+
+  // no pins for microstepping on GT2560 - must be disabled because some map to pins GT2560 uses for thermistors.
+ // #define X_MS1_PIN 40
+ // #define X_MS2_PIN 41
+ // #define Y_MS1_PIN 69
+ // #define Y_MS2_PIN 39
+ // #define Z_MS1_PIN 68
+ // #define Z_MS2_PIN 67
+ // #define E0_MS1_PIN 65
+ // #define E0_MS2_PIN 66
+  
+  // The SDSS pin uses a different pin mapping from file Sd2PinMap.h
+  #define SDSS               53 //
+
+  #ifndef SDSUPPORT
+    // these pins are defined in the SD library if building with SD support
+    #define SCK_PIN          52 //
+    #define MISO_PIN         50 //
+    #define MOSI_PIN         51 //
+  #endif
+  
+  #define BEEPER 18 //
+
+  #define BTN_EN1 42 // Swap with BTN_EN2 in case doesn't work ??
+  #define BTN_EN2 40 //
+  #define BTN_ENC 19 //
+
+  #define SDCARDDETECT 38 //
+  
+  #define LCD_PINS_RS 20 //
+  #define LCD_PINS_ENABLE 17 //
+  #define LCD_PINS_D4 16 //
+  #define LCD_PINS_D5 21 //
+  #define LCD_PINS_D6 5 //
+  #define LCD_PINS_D7 6 //
+  
+  #define E0_STEP_PIN         43 //
+  #define E0_DIR_PIN          45 //
+  #define E0_ENABLE_PIN       41 //
+
+  #define LED_PIN            13 // TODO what is this for? //
+  #define KILL_PIN           -1 //80 with Smart Controller LCD //
+  #define SUICIDE_PIN        54  //PIN that has to be turned on right after start, to keep power flowing. //
+  #define SDPOWER            -1 //
+
+
+  #define HEATER_0_PIN 2 //
+  #define HEATER_1_PIN 3 //
+  #define HEATER_2_PIN -1
+  #define HEATER_BED_PIN 4 //
+
+  // temperature pins
+  #define TEMP_BED_PIN 10 //
+  #define TEMP_0_PIN 8 //
+  #define TEMP_1_PIN 9 //
+  #define TEMP_2_PIN -1
+
+
+  // TODO figure out fans
+  #define FAN_PIN 7 //
+  #define FAN_1_PIN -1 //
+  #define PS_ON_PIN 12 //
+
+// The Z2 axis, if any, should be the next open extruder port
+#ifdef Z_DUAL_STEPPER_DRIVERS
+
+    #define Z2_STEP_PIN   36
+    #define Z2_DIR_PIN    34
+    #define Z2_ENABLE_PIN 30
+ 
+#endif
+  // looks like digitpot stuff all hangs off #ifdef MOTOR_CURRENT_PWM_XY_PIN
+  //#define MOTOR_CURRENT_PWM_XY_PIN 46
+  //#define MOTOR_CURRENT_PWM_Z_PIN 45
+  //#define MOTOR_CURRENT_PWM_E_PIN 44
+  
+#endif
 
 
 #ifndef KNOWN_BOARD
