b6edebdea32eea7d45c962c1c737cb8b0420caf0
PabloGNU <pablo@lanzate.com>
2018-01-08 17:38:41 +0100

    GT2560 REV A+ definition
    Add GT2560 REV A+ definition

--- Firmware/boards.h
+++ Firmware/boards.h
@@ -9,8 +9,8 @@
 #define BOARD_RAMBO_MINI_1_0    102  // Rambo-mini 1.0
 
 #define BOARD_MKS_BASE          40   // MKS BASE 1.0
+#define BOARD_GT2560_REV_A_PLUS 75   // Geeetech GT2560 Rev. A+ (with auto level probe)
 
-#define BOARD_99                99   // This is in pins.h but...?
 
 #define MB(board) (MOTHERBOARD==BOARD_##board)
 #define IS_RAMPS (MB(RAMPS_OLD) || MB(RAMPS_13_EFB) || MB(RAMPS_13_EEB) || MB(RAMPS_13_EFF) || MB(RAMPS_13_EEF))
